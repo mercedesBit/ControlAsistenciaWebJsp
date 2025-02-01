fb3eaf10a6487dbf7490cf628922a8e1474788be
Leonardo Pereyra P <i202401202@cibertec.edu.pe>
2025-02-01 02:58:14 -0500

    actualizacion 2

--- src/main/java/servlets/CursoServletDAO.java
+++ src/main/java/servlets/CursoServletDAO.java
@@ -17,11 +17,14 @@ import java.awt.Graphics2D;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.RenderingHints;
-import java.util.Base64;
 
 @WebServlet("/CursoReporteServlet")
 public class CursoServletDAO extends HttpServlet {
-    private Curso_DAO_interface cursoDAO = new CursoDAO();
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Curso_DAO_interface cursoDAO = new CursoDAO();
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
